c7c53b67aefadb8fbb1028a33630e1f230728c1b
Will <will@containerlabs.io>
2022-11-01 14:05:48 -0700

    force tostring...

--- src/helm_values.tf
+++ src/helm_values.tf
@@ -17,7 +17,7 @@ locals {
         memory = "${var.database_configuration.memory_limit}Gi"
       }
       limits = {
-        cpu    = "${var.database_configuration.cpu_limit}"
+        cpu    = tostring(var.database_configuration.cpu_limit)
         memory = "${var.database_configuration.memory_limit}Gi"
       }
     }
